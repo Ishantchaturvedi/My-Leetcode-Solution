f8e9820ba576945ea71f5b1be92be942f758bdcd
Ishant kumar <110047012+Ishantchaturvedi@users.noreply.github.com>
2025-07-28 06:36:27 +0530

    Add files via upload

--- 2044. Count Number of Maximum Bitwise-OR Subsets.cpp
+++ 2044. Count Number of Maximum Bitwise-OR Subsets.cpp
@@ -1,46 +1,24 @@
 class Solution {
 public:
-    void subsetsUtil(vector<int>& A, vector<vector<int> >& res,vector<int>& subset, int index)
-    {
-        res.push_back(subset);
-        for (int i = index; i < A.size(); i++) {
-            subset.push_back(A[i]);
-            subsetsUtil(A, res, subset, i + 1);
-            subset.pop_back();
-        }
-    
-        return;
-    }
-
-    vector<vector<int> > subsets(vector<int>& A)
-    {
-        vector<int> subset;
-        vector<vector<int> > res;
-        int index = 0;
-        subsetsUtil(A, res, subset, index);
-    
-        return res;
+    int n;
+    unsigned max_OR;
+    int f(int i, unsigned acc_or, vector<int>& nums){
+        if (i<0) return (acc_or==max_OR)?1:0;
+        int skip=f(i-1, acc_or, nums);
+        int take=f(i-1, acc_or| nums[i], nums);
+        return skip+take;
     }
     int countMaxOrSubsets(vector<int>& nums) {
-        int i, j, x_or=0, max=0, count=0;
-        vector<vector<int> > res = subsets(nums);
-        for(i=0 ; i<res.size() ; i++)
-        {
-            x_or = 0;
-            for(j=0 ; j<res[i].size() ; j++)
-            {
-                x_or |=res[i][j];
-            }
-            if(x_or>max)
-            {
-                max = x_or;
-                count=1;
-            }
-            else if(x_or==max)
-            {
-                count++;
-            }
-        }
-        return count;
+        n=nums.size();
+        max_OR=accumulate(nums.begin(), nums.end(), 0, bit_or<>());
+        return f(n-1, 0, nums);
     }
 };
+
+
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
