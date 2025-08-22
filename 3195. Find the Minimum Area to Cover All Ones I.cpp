0dd7256f7f39504e7c176ec26a31de9dab7e7c52
Ishant kumar <110047012+Ishantchaturvedi@users.noreply.github.com>
2025-08-22 07:26:18 +0530

    Add files via upload

---
+++ 3195. Find the Minimum Area to Cover All Ones I.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minimumArea(vector<vector<int>>& grid) {
+        int row = grid.size();
+        int col = grid[0].size();
+
+        int top = row, left = col;
+        int bottom = -1, right = -1;
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (grid[i][j] == 1) {
+                    top = min(top, i);
+                    bottom = max(bottom, i);
+                    left = min(left, j);
+                    right = max(right, j);
+                }
+            }
+        }
+
+        return (bottom - top + 1) * (right - left + 1);
+    }
+};
